944d0a92126bf410e23cce4bd72a986721011e7a
ma-syarikatislam <ma.syarikatislam@gmail.com>
2025-08-01 13:33:28 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function handleMenu(menuName) {
+  alert("Menu dipilih: " + menuName);
+  // Ganti dengan navigasi sebenarnya, misalnya:
+  // window.location.href = `./${menuName.replace(/\s/g, '_').toLowerCase()}.html`;
+}
